d5e5221169767d172ac9eae90e6dc3ca5b9169e9
tahir kalim <thrkalim@gmail.com>
2021-02-08 02:21:27 +0100

    jsx javascript

--- src/index.js
+++ src/index.js
@@ -8,20 +8,30 @@ function BookList(){
   return (
     <section className='booklist'>
       <Book/>
-      <Book/>
-      <Book/>
-      <Book/>
-      <Book/>
     </section>
   )
   
 }
-
+const author = 'Susan Wiggs'
 const Book = () => {
-  return <article className='book'><Image/><Title/><Author/></article>
-}
-const Image = () => <img src="https://m.media-amazon.com/images/I/81o3eY5E-rL._AC_UL320_.jpg" alt="" />
 
-const Title = () => <h1>Bestseller I love you to the Moon and Back</h1>
-const Author = () => <h3 style={{color: '#617d98', fontSize:'0.75rem',marginTop:'0.25rem'}}>Susan Wiggs</h3>
+  const title = 'Bestseller I love you to the Moon and Back'
+
+  return (
+
+  <article className='book'>
+   <img
+    src="https://m.media-amazon.com/images/I/81o3eY5E-rL._AC_UL320_.jpg" alt="" />
+   <h1>{title}</h1>
+
+   <h3 >{author.toUpperCase()}</h3>
+
+   <p>{6+6} </p>
+   
+    </article>)
+}
+// const Image = () => <img src="https://m.media-amazon.com/images/I/81o3eY5E-rL._AC_UL320_.jpg" alt="" />
+
+// const Title = () => <h1>Bestseller I love you to the Moon and Back</h1>
+// const Author = () => <h3 style={{color: '#617d98', fontSize:'0.75rem',marginTop:'0.25rem'}}>Susan Wiggs</h3>
 ReactDOM.render(<BookList />, document.getElementById('root'))
