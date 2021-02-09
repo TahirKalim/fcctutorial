da33426615c04445e8b68f8433a92adf54dd7d57
tahir kalim <thrkalim@gmail.com>
2021-02-10 00:58:21 +0100

    event basics

--- src/index.js
+++ src/index.js
@@ -40,13 +40,18 @@ function BookList(){
   )
 }
 
-const Book = (props) => {
-  const { img, title, author} = props
+const Book = ({img,title,author}) => {
+  //attribute, eventHandler
+  // onClick, onMouseOver
+  const clickHandler = () => {
+    alert('hello world');
+  }
   return (
-  <article className='book'>
+  <article className='book' onMouseOver={()=> { console.log(title)}}>
    <img src={img} alt="" />
    <h1>{title}</h1>
    <h3 >{author}</h3>
+   <button type='button' onClick={clickHandler}>Button</button>
     </article>)
 }
 
